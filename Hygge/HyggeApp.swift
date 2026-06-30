fa134457fc5d5b2465a71659aeeebd5921ad1028
Jesse <Jessewallacejohnson1@icloud.com>
2026-06-30 17:00:45 -0500

    Initial Commit

---
+++ Hygge/HyggeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HyggeApp.swift
+//  Hygge
+//
+//  Created by Owner on 6/30/26.
+//
+
+import SwiftUI
+
+@main
+struct HyggeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
